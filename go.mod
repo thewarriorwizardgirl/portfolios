be26f26e4c5c11412a2a9e6112806dcc95f21129
vidhiaggarwal <thewarriorwizardgirl@gmail.com>
2026-09-17 13:41:24 +0530

    portfolio web app commit 1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module manup1
+
+go 1.27.1
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	// })
+	// Serve static files from the static directory
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
+	http.HandleFunc("/", home)
+	http.HandleFunc("/about", about)
+
+	http.ListenAndServe(":80", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, "Hello, you've requested custom template home: %s\n", r.URL.Path)
+	renderTemplate(w, "home.html")
+
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	t, err := template.ParseFiles("templates/" + tmpl)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	//Execute the template to the writer because there is no error
+	t.Execute(w, nil)
+
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, "Hello, you've requested custom template about: %s\n", r.URL.Path)
+	renderTemplate(w, "about.html")
+}
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// )
+
+// func main() {
+// 	http.HandleFunc("/abc", home)
+// 	//Start the server on port
+// 	err := http.ListenAndServe(":8080", nil)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// }
+
+// func home(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+// 	//renderTemplate(w, "home.html")
+
+// }
+
+// func renderTemplate(w http.ResponseWriter, tmpl string) {
+// 	t, err := template.ParseFiles("templates/" + tmpl)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+// 	//Execute the template to the writer because there is no error
+// 	t.Execute(w, nil)
+
+// }
